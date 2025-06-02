34e681850f4f7cd241b0d0923d360e404620d4e5
joaoryan <sjoaoryan@gmail.com>
2025-06-02 14:24:54 -0300

    feat: swagger no ambiente de teste

--- backend/src/main/config/swagger.ts
+++ backend/src/main/config/swagger.ts
@@ -2,10 +2,7 @@ import type { Express } from 'express';
 import { serve, setup } from 'swagger-ui-express';
 import { noCache } from '../middlewares';
 import swaggerConfig from '../docs';
-import { env } from './env';
 
 export default (app: Express): void => {
-  if (env.NODE_ENV !== 'production') {
-    app.use('/docs', noCache, serve, setup(swaggerConfig));
-  }
+  app.use('/docs', noCache, serve, setup(swaggerConfig));
 };
